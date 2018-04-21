b7be39bc02818183624a938fb19d5e7dfd902f8a
Moon_Slight <moon-slight@users.noreply.github.com>
2018-04-21 22:32:33 +0800

    Initial commit

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour 
+{
+
+	public Button Play, Help, Exit, Back;
+	public GameObject HelpPanel;
+
+	// Use this for initialization
+	void Start () 
+	{
+		HelpPanel.SetActive (false);
+		Play.onClick.AddListener (PlayOnClick);
+		Help.onClick.AddListener (HelpOnClick);
+		Exit.onClick.AddListener (ExitOnClick);
+		Back.onClick.AddListener (BackOnClick);
+	}
+
+	void PlayOnClick()
+	{
+		SceneManager.LoadScene ("Play", LoadSceneMode.Single);
+		Debug.Log ("play\n");
+	}
+
+	void HelpOnClick()
+	{
+		HelpPanel.SetActive (true);
+		Debug.Log ("help\n");
+	}
+
+	void ExitOnClick()
+	{
+		Application.Quit ();
+		Debug.Log ("exit\n");
+	}
+
+	void BackOnClick()
+	{
+		HelpPanel.SetActive (false);
+		Debug.Log ("back\n");
+	}
+}
---
+++ Assets/Script/Play.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour 
+{
+
+	public Button Exit;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Exit.onClick.AddListener (ExitOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void ExitOnClick()
+	{
+		SceneManager.LoadScene ("MainMenu", LoadSceneMode.Single);
+		Debug.Log ("exit\n");
+	}
+}
